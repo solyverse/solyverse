3f0bd33c20c06ff93864e9f0889740f3fd91253d
solyverse <164720513+solyverse@users.noreply.github.com>
2024-03-26 19:59:28 +0800

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
 
       <div className="trailer">
 
-      <iframe src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2Fcarlo.perez.545%2Fvideos%2F1107956150516559%2F&show_text=false&width=560&t=0" width="560" height="300"  scrolling="no"  allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen></iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/-8atAUBnxjo?si=GJl06yW8yIvl4ozH" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
       </div>
 
       <div className='poster'>
